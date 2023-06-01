8bf59e829bddc0acffb84112113dcdb0a6a7fd12
Ali Ayman <96400739+AliAymanMohammed@users.noreply.github.com>
2023-06-01 17:32:22 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def buildJar() {
     echo "building the application..."
     sh 'mvn package'
-} 
+}
 
 def buildImage() {
     echo "building the docker image..."
@@ -10,10 +10,14 @@ def buildImage() {
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push aliaymanmohammed/demo-app:jma-2.0'
     }
-} 
+}
 
 def deployApp() {
     echo 'deploying the application...'
-} 
+    def dockerCmd = 'docker run -p 8080:8080 -d aliaymanmohammed/demo-app:jma-1.0 '
+    sshagent(['ubuntu-server-key']) {
+        sh "ssh -o StrictHostKeyChecking=no ubuntu@54.93.120.75 ${dockerCmd}"
+    }
+}
 
 return this
