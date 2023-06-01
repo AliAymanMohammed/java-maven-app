0df3a73ce085d0aed4a3d1c5aeb0ecdc2e2784f5
Ali Ayman <96400739+AliAymanMohammed@users.noreply.github.com>
2023-06-01 17:35:03 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'Docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t aliaymanmohammed/demo-app:jma-2.0 .'
+        sh 'docker build -t aliaymanmohammed/demo-app:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push aliaymanmohammed/demo-app:jma-2.0'
     }
@@ -14,7 +14,7 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
-    def dockerCmd = 'docker run -p 8080:8080 -d aliaymanmohammed/demo-app:jma-1.0 '
+    def dockerCmd = 'docker run -p 8080:8082 -d aliaymanmohammed/demo-app:jma-1.0 '
     sshagent(['ubuntu-server-key']) {
         sh "ssh -o StrictHostKeyChecking=no ubuntu@54.93.120.75 ${dockerCmd}"
     }
