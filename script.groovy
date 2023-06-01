f24dea0d1203a34f4150319efe701b67fb2def19
Ali Ayman <96400739+AliAymanMohammed@users.noreply.github.com>
2023-06-01 17:36:48 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'Docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t aliaymanmohammed/demo-app:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push aliaymanmohammed/demo-app:jma-2.0'
+        sh 'docker push aliaymanmohammed/demo-app:jma-1.0'
     }
 }
 
